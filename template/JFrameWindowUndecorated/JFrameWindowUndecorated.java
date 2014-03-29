dbbc69487522e59813e2ff13cba0424e24753674
pandineer <pandineer@gmail.com>
2014-03-29 15:17:59 +0900

    Add JFrameWindowUndecorated

---
+++ template/JFrameWindowUndecorated/JFrameWindowUndecorated.java
@@ -0,0 +1,37 @@
+package JFrameWindowUndecorated;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class JFrameWindowUndecorated extends JFrame {
+  private static String title = "Cheet Sheet - prototype";
+  private int windowSizeX = 640;
+  private int windowSizeY = 480;
+
+  // Constructor
+  public JFrameWindowUndecorated() {
+    // Set title
+    super(title);
+
+    // To close window
+    addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        System.exit(0);
+      }
+    });
+
+    // Initialize window
+    this.setSize(windowSizeX, windowSizeY);
+    this.setResizable(false);
+    this.setUndecorated(true);
+    this.setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    @SuppressWarnings("unused")
+    JFrameWindowUndecorated main = new JFrameWindowUndecorated();
+  }
+
+}
---
+++ template/JFrameWindowUndecorated/README.md
@@ -0,0 +1,6 @@
+#JFrameWindowUndecorated
+
+This source code creates undecorated JFrame window.  
+Created window has 640 x 480 size, and the window is non-resizable.  
+Window title is "JFrameWindow".  
+And you can exit this process by upper right close button.  
